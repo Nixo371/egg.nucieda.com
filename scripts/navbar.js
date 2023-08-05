5b15919dc0bd50364ab55e35a3592d13964b6a6b
Austin Coursey <acoursey3@murraystate.edu>
2023-08-05 14:51:22 -0500

    Add basic navbar and pages functionality

---
+++ scripts/navbar.js
@@ -0,0 +1,9 @@
+function toggleNavbar() {
+    var x = document.querySelector(".navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
